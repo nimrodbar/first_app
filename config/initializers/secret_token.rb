c47685a6803ba7d1b4f1fc7485dc829770970dd6
Nimrod Bar <nimrod@beecardia.com>
2012-12-19 16:28:27 +0200

    Initial ommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0a6c194674edbddb6e9f13d32a586f8a8fb24346f217d9ba1fe5e1466fbf389e44c0442180daf6f2a482f0a6e3f83a27e9252fb3af5cfdc4d981d671e71a9feb'
